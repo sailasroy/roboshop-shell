586861aa8d4a6c5c8d1d7d8818475c1bb3b89551
sailasroy <sailasroy123@gmail.com>
2023-08-22 12:38:31 +0530

    mongodb installation

---
+++ mongodb.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+    SCRIPT_NAME=$0
+    LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+    R="\e[31m"
+    G="\e[32m"
+    N="\e[0m"
+
+USERID=$(id -u)
+if [ $? -ne 0 ]
+then
+echo "ERROR:: Please sign in with root access"
+exit 1
+fi
+
+VALIDATE(){
+if ($1 -ne 0)
+then
+echo -e "$2 ........$R FAILURE $N"
+exit 1
+else 
+echo -e "$2............$G SUCCESS $N"
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &<<$LOGFILE
+VALIDATE $? "mongo.repo copied to yum.repos.d"
+yum install mongodb-org -y &<<$LOGFILE
+VALIDATE $? "mongodb installation"
+systemctl enable mongod &<<$LOGFILE
+VALIDATE $? "enabling mongod"
+systemctl start mongod &<<$LOGFILE
+VALIDATE $? "mongodb starting"
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &<<$LOGFILE
+VALIDATE $? "changing mongod.conf address"
+systemctl restart mongod &<<$LOGFILE
+VALIDATE $? "restarting mongodb"
+
+
+
+
+
+
+
+
