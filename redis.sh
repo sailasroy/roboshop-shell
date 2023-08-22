724d0219a54fa368ac743eb7488cb77f3530644a
sailasroy <sailasroy123@gmail.com>
2023-08-22 15:55:25 +0530

    REDIS Installation

--- redis.sh
+++ redis.sh
@@ -35,6 +35,7 @@ VALIDATE $? "Enabling Redis 6.2"
 yum install redis -y  &>>$LOGFILE
 VALIDATE $? "Installing Redis"
 
+#sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>>$LOGFILE
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>>$LOGFILE
 
 VALIDATE $? "Changing the address in redis.conf"
