5bc7480154d764fe3b7b21b027d192ca4feae112
sailasroy <sailasroy123@gmail.com>
2023-08-23 13:32:30 +0530

    Added Cart

--- cart.sh
+++ cart.sh
@@ -24,21 +24,21 @@ echo -e "$2............$G SUCCESS $N"
 fi
 }
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash 
     VALIDATE $? "Dowloading nodejs source file"
-yum install nodejs -y &>>$LOGFILE
+yum install nodejs -y 
     VALIDATE $? "Installing nodejs"  
 useradd roboshop
 
 mkdir /app
 
-curl -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>>$LOGFILE
+curl -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip 
     VALIDATE $? "Downloading the cart artifact"
 
-cd /app &>>$LOGFILE
+cd /app 
     VALIDATE $? "Opening app directory"
 
-unzip /tmp/cart.zip &>>$LOGFILE
+unzip /tmp/cart.zip 
     VALIDATE $? "Unzipping cart artifact"
 
 cd /app 
@@ -50,19 +50,19 @@ npm install
 cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
     VALIDATE $? "Copying cart.service"
 
-systemctl daemon-reload &>>$LOGFILE
+systemctl daemon-reload 
     VALIDATE $? "Reloading cart.service"
 
-systemctl enable cart &>>$LOGFILE
+systemctl enable cart 
     VALIDATE $? "Enabling cart.service"
 
-systemctl start cart &>>$LOGFILE
+systemctl start cart 
     VALIDATE $? "Starting cart.service"
 
-cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo 
     VALIDATE $? "Copying mongo.repo"
 
-yum install mongodb-org-shell -y &>>$LOGFILE
+yum install mongodb-org-shell -y 
     VALIDATE $? "Installing mongodb client"
 
 # mongo --host mongodb.sailasdevops.online </app/schema/cart.js &>>$LOGFILE
